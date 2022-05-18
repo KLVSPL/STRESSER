59e4c286617829b5b2cf54d185c0556b4484aaa5
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-05-19 06:13:46 +0800

    Update s4.py

--- s4.py
+++ s4.py
@@ -2,7 +2,7 @@ import requests
 out_file = "proxy.txt"
 f = open(out_file,'wb')
 socks4_api = [
-	"https://api.proxyscrape.com/?request=displayproxies&proxytype=socks4&timeout=550&country=all",
+	"https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=2000&country=all",
 ]
 for api in socks4_api:
 	try:
